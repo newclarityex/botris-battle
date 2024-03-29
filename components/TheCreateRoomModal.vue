6cdb8672cfd750f6382172fd545361588c09a700
newclarityex <69740123+newclarityex@users.noreply.github.com>
2024-03-29 16:18:29 -0400

    fixing create room

--- components/TheCreateRoomModal.vue
+++ components/TheCreateRoomModal.vue
@@ -33,29 +33,29 @@ async function createRoom(event: Event) {
         <div class="p-4">
             <h2 class="text-center">Create Room</h2>
             <form @submit.prevent="createRoom" class="flex flex-col items-center gap-4">
-                <div class="w-full gap-2 flex flex-row items-center justify-between">
+                <div class="w-full gap-4 flex flex-row items-center justify-between">
                     <label for="room-ft">Room FT</label>
                     <input type="number" id="room-ft" class="bg-black/40 w-12 px-1" required v-model="roomData.ft">
                 </div>
-                <div class="w-full gap-2 flex flex-row items-center justify-between">
+                <div class="w-full gap-4 flex flex-row items-center justify-between">
                     <label for="room-private">Room Private</label>
                     <input type="checkbox" id="room-private" class="bg-black/40" v-model="roomData.private">
                 </div>
-                <div class="w-full gap-2 flex flex-row items-center justify-between">
+                <div class="w-full gap-4 flex flex-row items-center justify-between">
                     <label for="initial-pps">Inital PPS (max: 30)</label>
-                    <input type="number" id="initial-pps" class="bg-black/40 w-12 px-1" required v-model="roomData.initialPps">
+                    <input type="number" id="initial-pps" class="bg-black/40 w-16 px-1" required v-model="roomData.initialPps" step="0.01">
                 </div>
-                <div class="w-full gap-2 flex flex-row items-center justify-between">
+                <div class="w-full gap-4 flex flex-row items-center justify-between">
                     <label for="final-pps">Final PPS (max: 30)</label>
-                    <input type="number" id="final-pps" class="bg-black/40 w-12 px-1" required v-model="roomData.finalPps">
+                    <input type="number" id="final-pps" class="bg-black/40 w-16 px-1" required v-model="roomData.finalPps" step="0.01">
                 </div>
-                <div class="w-full gap-2 flex flex-row items-center justify-between">
+                <div class="w-full gap-4 flex flex-row items-center justify-between">
                     <label for="start-margin">Start Margin (secs)</label>
-                    <input type="number" id="start-margin" class="bg-black/40 w-12 px-1" required v-model="roomData.startMargin">
+                    <input type="number" id="start-margin" class="bg-black/40 w-16 px-1" required v-model="roomData.startMargin">
                 </div>
-                <div class="w-full gap-2 flex flex-row items-center justify-between">
+                <div class="w-full gap-4 flex flex-row items-center justify-between">
                     <label for="end-margin">End Margin (secs)</label>
-                    <input type="number" id="end-margin" class="bg-black/40 w-12 px-1" required v-model="roomData.endMargin">
+                    <input type="number" id="end-margin" class="bg-black/40 w-16 px-1" required v-model="roomData.endMargin">
                 </div>
                 <button class="disabled:opacity-50 text-secondary" :disabled="disableCreate" type="submit">Submit</button>
             </form>
