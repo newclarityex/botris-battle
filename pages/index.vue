04d6f138e0733f9316604243d3c0aec75644a6e8
newclarityex <69740123+newclarityex@users.noreply.github.com>
2024-07-31 12:18:54 -0400

    fix page

--- pages/index.vue
+++ pages/index.vue
@@ -7,16 +7,31 @@
                 Botris Battle
             </h1>
             <h2 class="text-xl text-primary py-2">
+                <NuxtLink href="https://twitter.com/newclarityex" class="text-secondary">
+                    NukeExplosions
+                </NuxtLink> & 
                 <NuxtLink href="https://www.twitch.tv/puzzlekingdom" class="text-secondary">
                     PuzzleKingdom
-                </NuxtLink> & <NuxtLink href="https://twitter.com/newclarityex" class="text-secondary">
-                    NukeExplosions
                 </NuxtLink>
             </h2>
             <p class="text-xl text-content py-2 px-4 max-w-2xl indent-8">
                 Botris 2024 is a hackathon where teams of programmers compete to create the
                 best block-stacking AI in 6 months.
             </p>
+            <div class="py-6">
+                <h2 class="text-3xl text-primary py-8 underline text-center">Registration</h2>
+                <ol class="max-w-2xl list-decimal">
+                    <li>
+                        Register on <a class="text-secondary" target="_blank" href="https://challonge.com/botrisbattle">challonge</a>.
+                    </li>
+                    <li>
+                        Register on this website to test your bots.
+                    </li>
+                    <li>
+                        Join the <a class="text-secondary" target="_blank" href="https://discord.gg/NUSgpfT5eH">Nuketris discord</a>
+                    </li>
+                </ol>
+            </div>
             <div class="py-6">
                 <h2 class="text-2xl text-primary py-2 text-center">Rules</h2>
                 <ol class="max-w-2xl list-decimal">
@@ -36,7 +51,7 @@
                         If you heavily borrow from an existing bot that you did not write you must credit the original author(s).
                     </li>
                     <li>
-                        You must be in the Nuketris discord <a class="text-secondary" href="https://discord.gg/NUSgpfT5eH" target="_blank">here</a> and registered on Challonge <a class="text-secondary" href="https://challonge.com/botrisbattle" target="_blank">here</a> to participate. 
+                        You must be in the Nuketris discord <a class="text-secondary" target="_blank" href="https://discord.gg/NUSgpfT5eH">here</a> and registered on Challonge <a class="text-secondary" href="https://challonge.com/botrisbattle" target="_blank">here</a> to participate. 
                     </li>
                     <li>
                         You must not be banned from PuzzleKingdom events.
@@ -46,9 +61,6 @@
                     </li>
                 </ol>
             </div>
-            <NuxtLink to="https://discord.gg/NUSgpfT5eH" class="text-2xl text-primary py-8 underline">
-                Register for Botris Battle 2024
-            </NuxtLink>
             <!-- <button class="text-2xl text-primary underline py-6" @click="signIn('github')">Click Here to
                 Participate</button> -->
         </main>
